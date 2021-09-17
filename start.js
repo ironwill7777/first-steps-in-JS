fae4874eaff0f6463c2840070ae5576aa9126866
ironwill7777 <84805427+ironwill7777@users.noreply.github.com>
2021-09-17 09:48:26 +0300

    Add files via upload

---
+++ start.js
@@ -0,0 +1,7 @@
+const newBoolNumber = 4>1;
+const newBoolNumber2 = 4<1;
+const ordinalNumber = 7;
+const newBoolNumber1 = Number(ordinalNumber);
+console.log(newBoolNumber);
+console.log(newBoolNumber2);
+console.log(ordinalNumber);
